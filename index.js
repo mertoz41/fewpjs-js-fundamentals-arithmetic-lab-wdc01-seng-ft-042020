94f581cf20ef9a91f8b94ac0803244273a733d0c
mertoz41 <mertozkaynak@windowslive.com>
2020-05-31 19:33:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let set = [1,2,3];
+
+var multiply = num1 * num2;
+var random = Math.random();
+var mod = 4 % 10;
+var max = Math.max(1,2,20);
